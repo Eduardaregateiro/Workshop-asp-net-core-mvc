02cbeffe9d4d0fdf6892dc142383ffe65f38b57a
Eduarda <regateiroduda@msn.com>
2025-09-29 17:23:24 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,7 +6,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -29,20 +29,22 @@ namespace SalesWebMvc.Controllers
             return View(list);
         }
 
-        public IActionResult Create() 
+        public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
-                return View(viewModel);
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        
-        public IActionResult Create(Seller seller) {
+
+        public IActionResult Create(Seller seller)
+        {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Delete(int? id) {
+        public IActionResult Delete(int? id)
+        {
             if (id == null)
             {
                 return NotFound();
@@ -53,7 +55,7 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
             return View(obj);
-            }
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
@@ -75,6 +77,44 @@ namespace SalesWebMvc.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
-    
 }
